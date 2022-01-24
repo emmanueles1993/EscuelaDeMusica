864705223bd71251315606b26fff4932cfc1a710
emmanueles1993 <emmanueles1993@hotmail.com>
2022-01-24 17:30:53 -0500

    marimbafuncionalidad

---
+++ js/marimba.js
@@ -0,0 +1,24 @@
+const dataKeys= [
+    "z", "x", "c", "v", "b", "n", "m", ",", ".", "-"
+];
+const keys= document.querySelectorAll(".bar");
+
+
+keys.forEach((key)=>{
+    key.addEventListener("click",()=>{
+        playNote(key)
+    })
+})
+ 
+document.addEventListener("keydown",(e)=>{
+    const data= e.key 
+    const keyIndex= dataKeys.indexOf(data)
+  if(keyIndex >= 0 ){
+      playNote(keys[keyIndex])
+  }
+})
+
+function playNote(key){
+const noteAudio= document.getElementById(key.dataset.note);
+noteAudio.currentTime=0; noteAudio.play();
+}
