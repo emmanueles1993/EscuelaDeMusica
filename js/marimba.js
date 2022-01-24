00965979c2e163a6031a15ee3b08505072f7dc4b
christian lazo <kenishi.cristianxd@gmail.com>
2022-01-24 17:43:16 -0500

    marimbadetalles01

--- js/marimba.js
+++ js/marimba.js
@@ -1,24 +1,22 @@
-const dataKeys= [
-    "z", "x", "c", "v", "b", "n", "m", ",", ".", "-"
-];
-const keys= document.querySelectorAll(".bar");
+const dataKeys = ["z", "x", "c", "v", "b", "n", "m", ",", ".", "-"];
+const keys = document.querySelectorAll(".bar");
 
+keys.forEach((key) => {
+  key.addEventListener("click", () => {
+    playNote(key);
+  });
+});
 
-keys.forEach((key)=>{
-    key.addEventListener("click",()=>{
-        playNote(key)
-    })
-})
- 
-document.addEventListener("keydown",(e)=>{
-    const data= e.key 
-    const keyIndex= dataKeys.indexOf(data)
-  if(keyIndex >= 0 ){
-      playNote(keys[keyIndex])
+document.addEventListener("keydown", (e) => {
+  const data = e.key;
+  const keyIndex = dataKeys.indexOf(data);
+  if (keyIndex >= 0) {
+    playNote(keys[keyIndex]);
   }
-})
+});
 
-function playNote(key){
-const noteAudio= document.getElementById(key.dataset.note);
-noteAudio.currentTime=0; noteAudio.play();
+function playNote(key) {
+  const noteAudio = document.getElementById(key.dataset.note);
+  noteAudio.currentTime = 0;
+  noteAudio.play();
 }
