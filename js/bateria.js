47fdbe70bbfc9a14ce0684c1f1df43dfb271050e
christian lazo <kenishi.cristianxd@gmail.com>
2022-01-24 12:58:14 -0500

    funcionalidadBateria

--- js/bateria.js
+++ js/bateria.js
@@ -1,3 +1,4 @@
+const keys = ["w", "p", "s", "g", "l", "x", "b", ","];
 const drums = document.querySelectorAll(".bateria-content-img");
 
 drums.forEach((drum) => {
@@ -6,8 +7,24 @@ drums.forEach((drum) => {
   });
 });
 
+document.addEventListener("keydown", (e) => {
+  const key = e.key;
+
+  const keyIndex = keys.indexOf(key);
+
+  if (keyIndex >= 0) {
+    playSound(drums[keyIndex]);
+  }
+});
+
 function playSound(drum) {
   const audioDrum = document.getElementById(drum.dataset.sound);
   audioDrum.currentTime = 0;
   audioDrum.play();
+
+  drum.classList.add("active-bateria");
+
+  setTimeout(() => {
+    drum.classList.remove("active-bateria");
+  }, 200);
 }
