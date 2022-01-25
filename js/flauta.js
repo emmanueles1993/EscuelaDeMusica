a3f9e4049fe1be7c662d874bb48b1ae4771a5e69
christian lazo <kenishi.cristianxd@gmail.com>
2022-01-25 12:50:02 -0500

    flautacentrada

--- js/flauta.js
+++ js/flauta.js
@@ -1,24 +1,24 @@
-const keys=["z", "x", "c" , "v", "b", "n", "m", ","];
-const circles=document.querySelectorAll(".circle");
+const keys = ["z", "x", "c", "v", "b", "n", "m", ","];
+const circles = document.querySelectorAll(".circle");
 
-circles.forEach(circle => {
-    circle.addEventListener("click",()=>{
-playNote(circle)
-    })
+circles.forEach((circle) => {
+  circle.addEventListener("click", () => {
+    playNote(circle);
+  });
 });
-document.addEventListener("keydown",(e)=>{
-const key= e.key;
-const keyIndex= keys.indexOf(key);
-if(keyIndex >= 0){
+document.addEventListener("keydown", (e) => {
+  const key = e.key;
+  const keyIndex = keys.indexOf(key);
+  if (keyIndex >= 0) {
     playNote(circles[keyIndex]);
-}
-})
-function playNote(circle){
-    const audioFlauta= document.getElementById(circle.dataset.note)
-audioFlauta.currentTime= 0;
-audioFlauta.play();
-circle.classList.add("flauta-active");
-setTimeout(() => {
+  }
+});
+function playNote(circle) {
+  const audioFlauta = document.getElementById(circle.dataset.note);
+  audioFlauta.currentTime = 0.5;
+  audioFlauta.play();
+  circle.classList.add("flauta-active");
+  setTimeout(() => {
     circle.classList.remove("flauta-active");
-}, 200);
+  }, 400);
 }
