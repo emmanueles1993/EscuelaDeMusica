8f575022f1f04bf9fa1e9925a8f0b411f414b915
emmanueles1993 <emmanueles1993@hotmail.com>
2022-01-25 12:17:41 -0500

    flautafuncionalidad

---
+++ js/flauta.js
@@ -0,0 +1,20 @@
+const keys=["z", "x", "c" , "v", "b", "n", "m", ","];
+const circles=document.querySelectorAll(".circle");
+
+circles.forEach(circle => {
+    circle.addEventListener("click",()=>{
+playNote(circle)
+    })
+});
+document.addEventListener("keydown",(e)=>{
+const key= e.key;
+const keyIndex= keys.indexOf(key);
+if(keyIndex >= 0){
+    playNote(circles[keyIndex]);
+}
+})
+function playNote(circle){
+    const audioFlauta= document.getElementById(circle.dataset.note)
+audioFlauta.currentTime= 0;
+audioFlauta.play();
+}
